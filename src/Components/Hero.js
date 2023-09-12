60b70f657c579ccd8f81de74cdb46e7ba434d1b7
amir <mashhadifarahani1@gmail.com>
2023-09-12 15:35:34 +0330

    first commit

---
+++ src/Components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero(){
+    return(
+        <main className='hero'>
+            <img className="hero--image" src="images/photo-grid.png"/>
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+        </main>
+    )
+}
