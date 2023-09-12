bd8165373dc7a50a9795d3c03f650dcb103267ef
amir <mashhadifarahani1@gmail.com>
2023-09-12 16:29:16 +0330

    card design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import Navbar from './Components/Navbar';
-import Hero from './Components/Hero';
+import Card from './Components/Card';
 function App() {
   return(
     <div className='container'>
       <Navbar/>
-      <Hero/>
+      <Card />
     </div>
   )
 }
