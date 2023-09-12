57f637f507e47f426cd22ed4faca9597c1c16aec
amir <mashhadifarahani1@gmail.com>
2023-09-12 18:34:40 +0330

    using props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import Navbar from './Components/Navbar';
 import Card from './Components/Card';
+import Contact from './Components/Contact';
 function App() {
   return(
     <div className='container'>
       <Navbar/>
       <Card />
+
+      <Contact
+        img="images/images/mr-whiskerson.png"
+        name="Mr. Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <Contact
+        img="images/images/fluffykins.png"
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+      />
+      <Contact
+        img="images/images/felix.png"
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
+      />
+      
+
     </div>
   )
 }
